ff82d831f9758cac2e60f6b664e2a1fb94c16343
skingori <infor.samson@gmail.com>
2024-12-10 18:26:04 +0300

    New update

---
+++ src/test/java/com/restassured/ApiTests.java
@@ -0,0 +1,90 @@
+package com.restassured;
+
+import java.util.List;
+import java.util.Random;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+import io.restassured.response.Response;
+
+public class ApiTests {
+
+    private static final String BASE_URL = "https://reqres.in/api";
+
+    @BeforeAll
+    static void setup() {
+        RestAssured.baseURI = BASE_URL;
+    }
+
+    @Test
+    public void createUsers() {
+        given()
+                .contentType("application/json")
+                .body("{\n"
+                        + "    \"name\": \"morpheus\",\n"
+                        + "    \"job\": \"leader\"\n"
+                        + "}")
+                .when()
+                .post("/users")
+                .then()
+                .statusCode(201);
+    }
+
+    @Test
+    public void retrieveSingleUser() {
+        given()
+                .when()
+                .get("/users/" + getRandomUserIDByKey())
+                .then()
+                .statusCode(200);
+
+    }
+
+    @Test()
+    public void updateAnExistingUser() {
+        given()
+                .contentType("application/json")
+                .body("{\n"
+                        + "    \"name\": \"morpheus\",\n"
+                        + "    \"job\": \"zion resident\"\n"
+                        + "}")
+                .when()
+                .put("/users/" + getRandomUserIDByKey())
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void deleteAnExistingUser() {
+        given()
+                .when()
+                .delete("/users/" + getRandomUserIDByKey())
+                .then()
+                .statusCode(204);
+    }
+
+    @Test
+    public void retrieveANonExistingUser() {
+
+        given()
+                .when()
+                .get("/users/" + getRandomUserIDByKey() + 404)
+                .then()
+                .statusCode(404);
+    }
+
+
+    public String getRandomUserIDByKey() {
+        Response response = given()
+                .when()
+                .get("/users");
+        List<String> ids = response.path("data.id");
+        Random random = new Random();
+        int randomIndex = random.nextInt(ids.size());
+        return String.valueOf(ids.get(randomIndex));
+    }
+
+}
